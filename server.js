7ae1f287bb06553509dd3b1c33cb3832f27bb18e
your_name <HeavyMetalCookies@GMail.com>
2022-07-03 04:41:55 -0400

    [AAA.004]

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
- 
 
     const l_url = require( "url" );
 
     require( "http" ).createServer(function(i_ask,i_giv){       // [AAA.001]
                                                                 // [AAA.001]
         var sob ={
+
             m_ask : i_ask
         ,   m_giv : i_giv
-        ,   m_url : l_url.parse( i_ask.url , 1 ).pathname
+        ,   m_url : l_url.parse( i_ask.url , 1 ).pathname   
 
         };;
 
-        sob.m_giv.end( sob.m_uxrl );dasd
+        sob.m_giv.end( sob.m_url ); /**F1 BITCH*/
                                                                 // [AAA.001]
     }).listen( process.env.PORT );                              // [AAA.001]
 
