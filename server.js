a401616fb79ef1cc170ec4f7054753cff7bed9c2
your_name <HeavyMetalCookies@GMail.com>
2022-07-03 04:45:34 -0400

    [AAA.004]

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 
-    const l_url = require( "url" );
+    const l_url = require( "url" );                             // [AAA.004]
 
     require( "http" ).createServer(function(i_ask,i_giv){       // [AAA.001]
                                                                 // [AAA.001]
-        var sob ={
-
-            m_ask : i_ask
-        ,   m_giv : i_giv
-        ,   m_url : l_url.parse( i_ask.url , 1 ).pathname   
-
-        };;
-
-        sob.m_giv.end( sob.m_url ); /**F1 BITCH*/
+        var sob ={                                              // [AAA.004]
+                                                                // [AAA.004]
+            m_ask : i_ask                                       // [AAA.004]
+        ,   m_giv : i_giv                                       // [AAA.004]
+        ,   m_url : l_url.parse( i_ask.url , 1 ).pathname       // [AAA.004]
+                                                                // [AAA.004]
+        };;                                                     // [AAA.004]
+                                                                // [AAA.004]
+        sob.m_giv.end( sob.m_url ); /**F1 BITCH**/              // [AAA.004]
                                                                 // [AAA.001]
     }).listen( process.env.PORT );                              // [AAA.001]
 
