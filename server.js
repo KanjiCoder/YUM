5a112b081c38cce417939f23f880bb938f3fa177
your_name <HeavyMetalCookies@GMail.com>
2022-07-07 19:51:12 -0400

    [AAA.006]

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 
-const l_url = require( "url" );                                 // [AAA.004]
-                                                                
+const l_url = require( "url" );                                 // [AAA.004]  
+    
+    //:mimetypes_4_reasons:------------------------------://    // [AAA.006]
+                                                                // [AAA.006]
+    var d_jso = { "Content-Type": "application/json"     } ;    // [AAA.006]
+    var d_txt = { "Content-Type": "text/plain"           } ;    // [AAA.006]
+    var d_png = { "Content-Type": "image/png"            } ;    // [AAA.006]
+    var d_htm = { "Content-Type": "text/html"            } ;    // [AAA.006]
+    var d_js  = { "Content-Type": "text/javascript"      } ;    // [AAA.006]
+                                                                // [AAA.006]
+    //:------------------------------:mimetypes_4_reasons://    // [AAA.006]
+                                      
 require( "http" ).createServer(function(i_ask,i_giv){           // [AAA.001]
                                                                 // [AAA.001]
     var sob ={                                                  // [AAA.004]
